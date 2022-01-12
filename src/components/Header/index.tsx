aab91e90795629510a945fbec6b64bfb715a77c8
EduardoReisUX <ducabruno09@hotmail.com>
2022-01-12 15:33:18 -0400

    feat: add icon in header if it's in continent page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,57 @@
-import { Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Icon,
+  Image,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
+
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 export function Header() {
+  const route = useRouter();
+
   return (
-    <Flex
-      alignItems={"center"}
-      justifyContent={"center"}
-      width={"100%"}
-      py={{ base: 4, lg: 8 }}
-    >
-      <NextLink href={"/"} passHref>
-        <ChakraLink>
-          <Image
-            src={"/images/logo.svg"}
-            alt="logo"
-            h={{ base: "20px", lg: "46px" }}
-            px={4}
-          />
-        </ChakraLink>
-      </NextLink>
-    </Flex>
+    <Box width={"100%"} py={{ base: 4, lg: 8 }}>
+      <Container
+        display={"flex"}
+        maxW={"container.lg"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <NextLink href={"/"} passHref>
+          <ChakraLink>
+            <ChevronLeftIcon
+              w={{ base: 4, lg: 8 }}
+              h={{ base: 4, lg: 8 }}
+              color={"dark.heading"}
+              visibility={
+                route.pathname !== "/[continent]" ? "hidden" : "visible"
+              }
+            />
+          </ChakraLink>
+        </NextLink>
+
+        <NextLink href={"/"} passHref>
+          <ChakraLink>
+            <Image
+              src={"/images/logo.svg"}
+              alt="logo"
+              h={{ base: "20px", lg: "46px" }}
+              px={4}
+            />
+          </ChakraLink>
+        </NextLink>
+
+        <ChevronLeftIcon
+          w={{ base: 4, lg: 8 }}
+          h={{ base: 4, lg: 8 }}
+          visibility={"hidden"}
+        />
+      </Container>
+    </Box>
   );
 }
